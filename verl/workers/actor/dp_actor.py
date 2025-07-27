d3e45ac4e2d8c63d8ebdabbe2370b2c9f926f403
williamzhangNU <pingyuezhang2029@u.northwestern.edu>
2025-07-27 06:31:04 +0000

    update

--- verl/workers/actor/dp_actor.py
+++ verl/workers/actor/dp_actor.py
@@ -67,8 +67,8 @@ class DataParallelPPOActor(BasePPOActor):
         """
         response_length = micro_batch['responses'].size(-1)
         multi_modal_inputs = {}
-        image_flags = None
         if 'multi_modal_inputs' in micro_batch:
+            image_flags = None
             for key in micro_batch['multi_modal_inputs'][0].keys():
                 multi_modal_inputs[key] = torch.cat([inputs[key] for inputs in micro_batch['multi_modal_inputs']],
                                                     dim=0)
